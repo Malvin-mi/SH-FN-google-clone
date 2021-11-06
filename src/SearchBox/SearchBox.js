fff5df10f2167173d9c0a52669e8ced9c2e763fe
Shiroiii <42494614+Shiroiii@users.noreply.github.com>
2021-11-06 11:49:07 +0100

    update
    added and arranged the search icon

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './searchbox.css';
 const SearchBox = () => {
     return (
-        <div style={{position:"relative", width:"561px", height:"44px"}}>
+        <div id="search">
             <input 
-                className="pa3 ba b--green bg-lighest-blue" 
-                type='search' 
-                placeholder='Search Google or type in a URL' 
-                style={{width:"100%", height:"42px", paddingLeft:"52px", paddingRight:"44px", border:"none", outline:"none", borderRadius:"calc(0.5 * 44px)", boxSizing:"border-box", backgroundColor:"#EFEFEF"}}
+                id="search-input" 
+                type='text' 
+                autoFocus="true"
+                autoComplete="off"
             />
-            <button id="search"></button>
+            <span id="search-icon"></span>
+            <button id="search-voice-btn"></button>
         </div>
     )
 }
