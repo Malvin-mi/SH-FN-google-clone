da7dcd540756a245d83e1fb33d0c0b7222a4fe18
Shiroiii <42494614+Shiroiii@users.noreply.github.com>
2021-11-06 12:44:42 +0100

    #
    #

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBox from './SearchBox/SearchBox';
 import './App.css';
 
 function App() {
+  // To be used in changing Logo text
   const [logo, setLogo] = useState();
   const updateLogo = (event) => setLogo(event.target.value);
 
